471059ca31471f195f1ceb870f3aadac2c6e01b3
Dmitry <v3il@ukr.net>
2022-10-09 21:59:41 +0300

    Fix tests for user

--- src/models/__tests__/User.test.js
+++ src/models/__tests__/User.test.js
@@ -1,43 +1,35 @@
 import { User } from '../User';
 
-const DEFAULT_BIRTHDATE = new Date();
-
-const createUser = (overrides = {}) => new User({
-    id: 'qwerty',
-    name: 'Dmytro',
-    lastName: 'Kit',
-    avatarId: 5,
-    birthDate: DEFAULT_BIRTHDATE,
-    ...overrides
-});
+const createUser = (params = {}) => new User(params);
 
 describe('Constructor', () => {
     test('Should init the id property', () => {
-        const user = createUser();
+        const user = createUser({ id: 'qwerty' });
 
         expect(user.id).toBe('qwerty');
     });
 
     test('Should init the avatarId property', () => {
-        const user = createUser();
+        const user = createUser({ avatarId: 5 });
 
         expect(user.avatarId).toBe(5);
     });
 
     test('Should init the birthDate property', () => {
-        const user = createUser();
+        const birthDate = new Date();
+        const user = createUser({ birthDate });
 
-        expect(user.birthDate).toBe(DEFAULT_BIRTHDATE);
+        expect(user.birthDate).toBe(birthDate);
     });
 
     test('Should init the fullName property', () => {
-        const user = createUser();
+        const user = createUser({ name: 'Dmytro', lastName: 'Kit' });
 
         expect(user.fullName).toBe('Dmytro Kit');
     });
 
     test('Should init the postsCount property', () => {
-        const user = createUser();
+        const user = createUser({ avatarId: 5 });
 
         expect(user.postsCount).toBe(125);
     });
@@ -45,13 +37,23 @@ describe('Constructor', () => {
 
 describe('getData', () => {
     test('Should init the id property', () => {
-        const user = createUser();
+        const birthDate = new Date();
+        const user = createUser({
+            id: 'qwerty',
+            name: 'Dmytro',
+            lastName: 'Kit',
+            avatarId: 5,
+            birthDate
+        });
+
+        const f = (n) => (n < 10 ? `0${n}` : n);
+        const formattedDate = `${f(birthDate.getDate())}/${f(birthDate.getMonth() + 1)}/${birthDate.getFullYear()}`;
 
         expect(user.getData()).toBe({
             id: 'qwerty',
             fullName: 'Dmytro Kit',
             postsCount: 125,
-            birthDate: '09/10/2022',
+            birthDate: formattedDate,
             avatarSrc: 'https://ik.imagekit.io/igo1qzk1oe2z/avatars/5.webp'
         });
     });
