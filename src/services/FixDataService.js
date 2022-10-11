8e1ff194455335f00c88990a212b9ca48d1948e7
Dmitry <v3il@ukr.net>
2022-10-11 22:03:57 +0300

    Tests for fix data service

--- src/services/FixDataService.js
+++ src/services/FixDataService.js
@@ -1,5 +1,13 @@
 export class FixDataService {
     fixData(brokenUsersJson) {
+        return this.parseUsersJson(brokenUsersJson).map(this.fixUser);
+    }
+
+    parseUsersJson(usersJSON) {
         return [];
+    }
+
+    fixUser(brokenUser) {
+        return brokenUser;
     }
 }
---
+++ src/services/__tests__/FixDataService.test.js
@@ -0,0 +1,43 @@
+import { FixDataService } from '../FixDataService';
+
+const createService = () => new FixDataService();
+
+describe('parseUsersJson', () => {
+    test('Should correctly parse users data', () => {
+        const user = {
+            id: ['😎', '      test          '],
+            name: 'Dmytro__Dmytro__Dmytro',
+            lastName: 'kIT',
+            avatarId: { value: 9 },
+            birthDate: 123456
+        };
+
+        const parsedUsers = createService().parseUsersJson(JSON.stringify([user]));
+
+        expect(parsedUsers).toStrictEqual([user]);
+    });
+});
+
+describe('fixUser', () => {
+    test('Should correctly fix user\'s data', () => {
+        const birthDate = new Date();
+
+        const user = {
+            id: ['😎', '      test          '],
+            name: 'Dmytro__Dmytro__Dmytro',
+            lastName: 'kIT',
+            avatarId: { value: 9 },
+            birthDate: Math.floor(birthDate.getTime() / 1000 / 2.25)
+        };
+
+        const fixedUser = createService().fixUser(user);
+
+        expect(fixedUser).toStrictEqual({
+            id: 'test',
+            name: 'Dmytro',
+            lastName: 'Kit',
+            avatarId: 3,
+            birthDate
+        });
+    });
+});
