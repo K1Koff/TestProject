fad8d8227cf7f80a19ce484bc2127adaca208d69
Dmitry <v3il@ukr.net>
2022-10-11 21:05:51 +0300

    Tests for users service

--- src/services/__tests__/UserService.test.js
+++ src/services/__tests__/UserService.test.js
@@ -35,7 +35,7 @@ describe('filterUsers', () => {
 });
 
 describe('sortUsers', () => {
-    test('Should filter users', () => {
+    test('Should sort users by name (asc)', () => {
         const users = [
             { fullName: 'Dmytro' },
             { fullName: 'Dmytryk' },
@@ -43,11 +43,103 @@ describe('sortUsers', () => {
             { fullName: 'Mitiay' }
         ];
 
-        const filteredUsers = createService().filterUsers(users, { query: 'dmy' });
+        const filteredUsers = createService().sortUsers(users, { sortBy: 'full_name', order: 'asc' });
 
         expect(filteredUsers).toStrictEqual([
             { fullName: 'Dmytro' },
-            { fullName: 'Dmytryk' }
+            { fullName: 'Dmytryk' },
+            { fullName: 'Mitiay' },
+            { fullName: 'Roman' }
+        ]);
+    });
+
+    test('Should sort users by name (desc)', () => {
+        const users = [
+            { fullName: 'Dmytro' },
+            { fullName: 'Dmytryk' },
+            { fullName: 'Roman' },
+            { fullName: 'Mitiay' }
+        ];
+
+        const filteredUsers = createService().sortUsers(users, { sortBy: 'full_name', order: 'desc' });
+
+        expect(filteredUsers).toStrictEqual([
+            { fullName: 'Roman' },
+            { fullName: 'Mitiay' },
+            { fullName: 'Dmytryk' },
+            { fullName: 'Dmytro' }
+        ]);
+    });
+
+    test('Should sort users by posts count (asc)', () => {
+        const users = [
+            { postsCount: 30 },
+            { postsCount: 111 },
+            { postsCount: 99 },
+            { postsCount: 77 },
+        ];
+
+        const filteredUsers = createService().sortUsers(users, { sortBy: 'posts', order: 'asc' });
+
+        expect(filteredUsers).toStrictEqual([
+            { postsCount: 30 },
+            { postsCount: 77 },
+            { postsCount: 99 },
+            { postsCount: 111 },
+        ]);
+    });
+
+    test('Should sort users by posts count (desc)', () => {
+        const users = [
+            { postsCount: 30 },
+            { postsCount: 111 },
+            { postsCount: 99 },
+            { postsCount: 77 },
+        ];
+
+        const filteredUsers = createService().sortUsers(users, { sortBy: 'posts', order: 'desc' });
+
+        expect(filteredUsers).toStrictEqual([
+            { postsCount: 111 },
+            { postsCount: 99 },
+            { postsCount: 77 },
+            { postsCount: 30 },
+        ]);
+    });
+
+    test('Should sort users by birth date (asc)', () => {
+        const users = [
+            { birthDate: new Date(1999, 0) },
+            { birthDate: new Date(1977, 0) },
+            { birthDate: new Date(2011, 0) },
+            { birthDate: new Date(1988, 0) },
+        ];
+
+        const filteredUsers = createService().sortUsers(users, { sortBy: 'birth_date', order: 'asc' });
+
+        expect(filteredUsers).toStrictEqual([
+            { birthDate: new Date(1977, 0) },
+            { birthDate: new Date(1988, 0) },
+            { birthDate: new Date(1999, 0) },
+            { birthDate: new Date(2011, 0) },
+        ]);
+    });
+
+    test('Should sort users by birth date (desc)', () => {
+        const users = [
+            { birthDate: new Date(1999, 0) },
+            { birthDate: new Date(1977, 0) },
+            { birthDate: new Date(2011, 0) },
+            { birthDate: new Date(1988, 0) },
+        ];
+
+        const filteredUsers = createService().sortUsers(users, { sortBy: 'birth_date', order: 'desc' });
+
+        expect(filteredUsers).toStrictEqual([
+            { birthDate: new Date(2011, 0) },
+            { birthDate: new Date(1999, 0) },
+            { birthDate: new Date(1988, 0) },
+            { birthDate: new Date(1977, 0) },
         ]);
     });
 });
